0e2ae830157bea37735c81dfbbfd99b51d9c8eb6
leejason28 <jason928lee@gmail.com>
2023-05-09 12:40:29 -0700

    implemented

--- sort.rb
+++ sort.rb
@@ -1,3 +1,15 @@
 def bubble_sort(array)
-  
+  while array != array.sort do
+    for ind in 0..array.length-2
+      if array[ind]>array[ind+1]
+        temp = array[ind]
+        array[ind] = array[ind+1]
+        array[ind+1] = temp
+      end
+    end
+  end
+  array
 end
+
+#arr = [4,3,78,2,0,2]
+#p bubble_sort(arr)
